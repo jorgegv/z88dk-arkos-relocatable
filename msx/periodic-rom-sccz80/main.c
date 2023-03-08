2712378482b2679dce172ca74a6c1f441229aeff
ZXjogv <zx@jogv.es>
2023-03-08 10:29:12 +0100

    msx: fix compilation using SCCZ80 (instead of SDCC)

--- msx/periodic-rom-sccz80/main.c
+++ msx/periodic-rom-sccz80/main.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 // Arkos C prototypes
-void ply_akg_init( void *song, unsigned int subsong ) __z88dk_callee;
+void ply_akg_init( void *song, unsigned int subsong ) __z88dk_callee __smallc;
 void ply_akg_play( void );
 void ply_akg_stop( void );
 void ply_akg_initsoundeffects( void *effects_table ) __z88dk_fastcall;
