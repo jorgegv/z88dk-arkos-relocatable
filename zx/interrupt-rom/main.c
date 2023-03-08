2819f7a500643d9042d5d7fec0af8a3b6a15f8f2
ZXjogv <zx@jogv.es>
2023-03-09 00:12:06 +0100

    refactored include, moved tests to base dir

--- zx/interrupt-rom/main.c
+++ zx/interrupt-rom/main.c
@@ -4,12 +4,7 @@
 #include <string.h>
 #include <z80.h>
 
-// Arkos C prototypes
-void ply_akg_init( void *song, unsigned int subsong ) __z88dk_callee __smallc;
-void ply_akg_play( void );
-void ply_akg_stop( void );
-void ply_akg_initsoundeffects( void *effects_table ) __z88dk_fastcall;
-void ply_akg_playsoundeffect( unsigned int effect ) __z88dk_fastcall;
+#include <arkos.h>
 
 extern uint8_t song[];
 
